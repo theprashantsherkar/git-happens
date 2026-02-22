eaff0afa565d0fd69ff04cfcbffaa59de71d8886
Akanksha Raina <akanksharainadjsce@gmail.com>
2026-02-22 10:05:31 +0530

    add credentials:true in cors setup of io instance in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import app from "./index.js";
 import { connectDB } from "./database/db.js";
-import { Server } from 'socket.io'
-import http from 'http'
+import { Server } from "socket.io";
+import http from "http";
 import registerSocketHandlers from "./socket/index.js";
 
 connectDB();
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"]
-    }
-})
+  cors: {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 
 registerSocketHandlers(io);
 
-app.listen(5000, () => {
-    console.log("Server up and running at port 5000")
-})
+server.listen(5000, () => {
+  console.log("Server up and running at port 5000");
+});
